784fb67dae3714d40aad460aaf7bd62398e706cd
Mugen <melton.sheridan@gmail.com>
2022-06-02 20:25:05 -0400

    updated schema/seeds files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,36 @@
 -- categories --
-INSERT INTO categories (name)
+INSERT INTO category (name)
 VALUE ('Demons');
-INSERT INTO categories (name)
+INSERT INTO category (name)
 VALUE ('Undead');
-INSERT INTO categories (name)
-VALUE ('Yokai');
-INSERT INTO categories (name)
-VALUE ('Fae');
-INSERT INTO categories (name)
-VALUE ('Aquatic');
-INSERT INTO categories (name)
+-- INSERT INTO categories (name) --
+-- VALUE ('Yokai'); --
+-- INSERT INTO categories (name)
+--VALUE ('Fae');
+--INSERT INTO categories (name)
+--VALUE ('Aquatic');
+INSERT INTO category (name)
 VALUE ('Mythical');
+
+
+INSERT INTO role (category_id)
+VALUE ("1");
+INSERT INTO role (category_id)
+VALUE ("2");
+INSERT INTO role (category_id)
+VALUE ("3");
+
+
+-- monster names/id --
+INSERT INTO monster (monster_name, role_id, )
+VALUE ("Vampire", 2);
+INSERT INTO monster (monster_name, role_id)
+VALUE ("Zombie", 2);
+INSERT INTO monster (monster_name, role_id)
+VALUE ("Astaroth",1);
+INSERT INTO monster (monster_name, role_id)
+VALUE ("Lucifer", 1 );
+INSERT INTO monster (monster_name, role_id)
+VALUE ("Mothman", 3);
+INSERT INTO monster (monster_name, role_id)
+VALUE ("Chupacabra", 3);
