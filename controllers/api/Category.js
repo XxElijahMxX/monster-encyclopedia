f4b0bc411c4bc04de4e1c605a8dc3efea39334b4
Mugen <melton.sheridan@gmail.com>
2022-06-06 18:51:29 -0400

    get route for selecting individual categories is working

--- controllers/api/Category.js
+++ controllers/api/Category.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
     })
     .then(categoryData => {
         if(!categoryData) {
-            res.status(404).json({ message: 'Requested category not found'});
+            res.status(404).json({ message: 'Requested category was not found'});
             return;
         }
         res.json(categoryData);
@@ -24,6 +24,31 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err)
     })
+});
+
+router.get('/:id', (req, res) => {
+    Category.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'category_name'],
+        include: [
+            {
+                model: Monsters,
+                attributes: ['category_id', 'id', 'monster_name']
+            }
+        ]
+    })
+    .then(categoryData => {
+        if(!categoryData) {
+            res.status(404).json({ message: 'Requested category was not found' });
+            return;
+        }
+        res.json(categoryData);
+    })
+    .catch(err => {
+        console.log(err)
+    });
 });
 
 
