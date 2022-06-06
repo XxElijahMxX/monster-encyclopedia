ea3179a6ff184f8da014578f3f06f2f9504fbd89
Mugen <melton.sheridan@gmail.com>
2022-06-06 19:03:43 -0400

    got get route for all monsters working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const categoryRoutes = require('./Category');
+const monsterRoutes = require('./monsters');
 
 
 
 
 router.use('/categories', categoryRoutes)
+router.use('/monsters', monsterRoutes)
 
 module.exports = router;
--- controllers/api/monsters.js
+++ controllers/api/monsters.js
@@ -1,4 +1,22 @@
 const router = require('express').Router();
+const { Monsters, Category } = require('../../models');
+
+router.get('/', (req, res) => {
+    Monsters.findAll({
+        attributes: ['id', 'category_id', 'monster_name'],
+        include: [
+            {
+                model: Category,
+                attributes: ['category_name', 'id']
+            },
+        ]
+    })
+    .then((monData) => res.json(monData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 
 
