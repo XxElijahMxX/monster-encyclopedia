aa5bc4197ef2c0ee44d04dfb68e735530a874d93
Mugen <melton.sheridan@gmail.com>
2022-06-07 17:57:26 -0400

    added put/post/delete routes for categories and they are working

--- controllers/api/Category.js
+++ controllers/api/Category.js
@@ -48,18 +48,58 @@ router.get('/:id', (req, res) => {
     })
     .catch(err => {
         console.log(err)
+        res.status(500).json(err)
+    });
+});
+
+router.post('/', (req, res) => {
+    Category.create({
+        category_name: req.body.category_name
+    })
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+    Category.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(categoryData => {
+        if (!categoryData[0]) {
+            res.status(404).json({ message: 'The category could not be found with this id'});
+            return;
+        }
+        res.json(categoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     });
 });
 
 
-
-
-
-
-
-
-
-
-
+router.delete('/:id', (req, res) => {
+    Category.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(categoryData => {
+        if(!categoryData) {
+            res.status(404).json({ message: 'The category could not be found with this id'});
+            return;
+        }
+        res.json(categoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
